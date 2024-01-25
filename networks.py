f42480bd5d4b0e65e1828c7f779ea9bf0a6e2fd6
Yizhak Y. Elboher <yizhak.elboher@mail.huji.ac.il>
2024-01-25 19:51:21 +0200

    networks.py: replace SkipConnection with FullyConnectedSkipConnection

--- networks.py
+++ networks.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+from copy import deepcopy
 
 class FullyConnected(nn.Module):
     def __init__(self, input_size, output_size, hidden_sizes):
@@ -16,16 +16,38 @@ class FullyConnected(nn.Module):
         return self.model(x)
 
 
-class SkipConnection(nn.Module):
-    def __init__(self, input_size, output_size, hidden_sizes):
-        super(SkipConnection, self).__init__()
-        layers = []
-        sizes = [input_size] + hidden_sizes + [output_size]
-        for i in range(len(sizes)-1):
-            layers.append(nn.Linear(sizes[i], sizes[i+1]))
-            if i < len(sizes)-2:
-                layers.append(nn.ReLU())
-        self.model = nn.Sequential(*layers)
+class FullyConnectedSkipConnection(nn.Module):
+    def __init__(
+        self, input_size, output_size, hidden_size=256, dup_or_init="dup"
+    ):
+        super(FullyConnectedSkipConnection, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        err_msg = f"invalid dup_or_init value: {dup_or_init}"
+        assert dup_or_init in ["dup", "init"], err_msg
+        self.dup_or_init = dup_or_init
+        self.model = None
+    
+    def add_layer(self):
+        if self.model is None:
+            self.model = nn.Sequential(
+                nn.Linear((self.input_size, self.hidden_size)),
+                nn.ReLU(),
+                nn.Linear((self.hidden_size, self.output_size)),
+            )
+        else:
+            # get the current layers
+            layers = [l for l in self.model]
+            new_layer = nn.Linear((self.hidden_size, self.hidden_size))
+            # add the new layer
+            if self.dup_or_init == "dup":
+                last_layer = deepcopy(layers[-1])
+            else:  # self.dup_or_init == "init":
+                last_layer = nn.Linear((self.hidden_size, self.output_size))
+            layers = layers[:-1] + [new_layer, nn.ReLU(), last_layer]
+            # create model
+            self.model = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.model(x)
@@ -33,7 +55,7 @@ class SkipConnection(nn.Module):
 
 network_type2network_class = {
     "fc": FullyConnected, 
-    "sc": SkipConnection
+    "fc_sc_clf": FullyConnectedSkipConnection
 }
 
 def network_factory(network_type, input_size, output_size, hidden_sizes):
